c111fb02879f00444d357843d5d0ccf410786a48
LeoBang <bluke8489@gmail.com>
2022-10-18 14:20:08 +0900

    [Config] Nier 프로젝트 생성 및 설정 (squash) (#2) <As Is> (squashed commit)  - main루트에 application.yml 설정    - DB 및 JPA 설정      - Postgres >> 로컬에 'nier' 이름 가진 DB 생성해주세요.      - Dev는 ddl-auto 전략: create  - SecurityConfig 설정    - default로 적용되는 spring security 무효화 <To Be>  - 동일

---
+++ src/main/java/com/ada/earthvalley/nier/NierApplication.java
@@ -0,0 +1,13 @@
+package com.ada.earthvalley.nier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NierApplication.class, args);
+	}
+
+}
