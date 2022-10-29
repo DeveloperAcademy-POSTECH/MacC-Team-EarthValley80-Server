0c70a6f19811fd689c72d796858dc0911f5771cd
LeoBang <bluke8489@gmail.com>
2022-10-30 00:05:05 +0900

    [Feat] JwtAuthenticationFilter 생성 및 FilterChain에 등록 (#22)

--- src/main/java/com/ada/earthvalley/yomojomo/configs/SecurityConfig.java
+++ src/main/java/com/ada/earthvalley/yomojomo/configs/SecurityConfig.java
@@ -1,14 +1,27 @@
 package com.ada.earthvalley.yomojomo.configs;
 
+import com.ada.earthvalley.yomojomo.auth.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .addFilterAt(
+                        jwtAuthenticationFilter,
+                        BasicAuthenticationFilter.class)
+                .authorizeRequests()
+                .anyRequest().authenticated()
+        ;
         return http.build();
     }
 }
