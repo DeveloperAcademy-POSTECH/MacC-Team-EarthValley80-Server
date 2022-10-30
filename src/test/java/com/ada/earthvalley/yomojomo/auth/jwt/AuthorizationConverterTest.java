bd44972e39de96addcc59952d6b90744e52d035c
LeoBang <bluke8489@gmail.com>
2022-10-30 23:37:25 +0900

    [Test] AuthorizationConverter 테스트 (#25) - convert 메서드 테스트   - 성공   - 실패     (case1) 헤더가 없음     (case2) header가 2개로 나뉘어지지 않음     (case3) header의 scheme이 bearer가 아님

---
+++ src/test/java/com/ada/earthvalley/yomojomo/auth/jwt/AuthorizationConverterTest.java
@@ -0,0 +1,68 @@
+package com.ada.earthvalley.yomojomo.auth.jwt;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+class AuthorizationConverterTest {
+    private static String AUTHORIZATION_HEADER = "Authorization";
+    private AuthorizationConverter converter;
+    private MockHttpServletRequest request;
+
+    @BeforeEach()
+    void init() {
+        converter = new AuthorizationConverter();
+        request = new MockHttpServletRequest();
+    }
+
+    @Test
+    @DisplayName(".convert() 실패 - header가 없음")
+    void convert_fail_header_null() throws Exception {
+        // when, then
+        assertThat(converter.convert(request)).isNull();
+    }
+
+    @Test
+    @DisplayName(".convert() 실패 - header가 두 덩이가 아님")
+    void convert_fail_header_not_two() {
+        // given
+        request.addHeader("Authorization", " bearer fefef fefef");
+
+        // when, then
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            converter.convert(request);
+        });
+        assertThat(exception.getMessage()).isEqualTo("올바르지 않은 형식");
+    }
+
+    @Test
+    @DisplayName(".convert() 실패 - header가 Bearer가 아님")
+    void convert_fail_not_bearer() {
+        // given
+        request.addHeader(AUTHORIZATION_HEADER, "Basic LeoBang");
+        // when, then
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            converter.convert(request);
+        });
+        assertThat(exception.getMessage()).isEqualTo("Bearer가 아님");
+    }
+
+    @Test
+    @DisplayName(".convert() 성공")
+    void convert_success() {
+        // given
+        request.addHeader(AUTHORIZATION_HEADER, "Bearer wefwefwefwefwe");
+        // when
+        JwtAuthenticationToken auth = converter.convert(request);
+        System.out.println(auth.getCredentials());
+
+        // then
+        assertThat(auth.getCredentials()).isEqualTo("wefwefwefwefwe");
+        assertThat(auth.isAuthenticated()).isFalse();
+    }
+}
