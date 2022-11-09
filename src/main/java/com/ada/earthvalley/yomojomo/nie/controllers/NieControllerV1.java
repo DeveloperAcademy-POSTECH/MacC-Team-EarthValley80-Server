4cda38ea0b411167c6a3b4a508c35c87f981a489
LeoBang <bluke8489@gmail.com>
2022-11-09 11:07:53 +0900

    [Feat] 단일 Nie 조회 API 구현 (#41) - controller, service, repository layer - FetchNieResponse: api response body 정의

---
+++ src/main/java/com/ada/earthvalley/yomojomo/nie/controllers/NieControllerV1.java
@@ -0,0 +1,30 @@
+package com.ada.earthvalley.yomojomo.nie.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ada.earthvalley.yomojomo.nie.services.NieFetchServiceV1;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+@RestController
+public class NieControllerV1 {
+	private final NieFetchServiceV1 nieFetchService;
+
+	@GetMapping("/nies/{nieId}")
+	public ResponseEntity fetch(
+		@PathVariable final Long nieId
+	) {
+		return ResponseEntity.ok(nieFetchService.fetchNie(nieId));
+	}
+
+	@GetMapping("/leo")
+	public String ab() {
+		return "으앙";
+	}
+}
---
+++ src/main/java/com/ada/earthvalley/yomojomo/nie/repositories/NieRepository.java
@@ -0,0 +1,8 @@
+package com.ada.earthvalley.yomojomo.nie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ada.earthvalley.yomojomo.nie.entities.Nie;
+
+public interface NieRepository extends JpaRepository<Nie, Long> {
+}
