99d5724914c1aee690c2d86320047d6dd1266de0
LeoBang <bluke8489@gmail.com>
2022-11-09 11:13:40 +0900

    [Chore] 기타 변경사항 (#41) - NieContext.java   - articleCursor column의 타입을 primitive -> wrapper 타입으로 변경   - 자세한 내용은 trouble shooting log 확인 (https://github.com/DeveloperAcademy-POSTECH/MacC-Team-EarthValley80-Server/wiki/@Basic-어노테이션.-그리고-primitive-type에-대해-not-null-제약조건을-매핑하는-이유) - NieProcess.java   - AllArgs 생성자 붙임 - NieProcessType.java   - 예측하기 단계의 네이밍 GUESSING -> PREDICT - GlobalExceptionHandlerTest.java   - scope을 테스트 대상 controller로 좁힘

--- src/test/java/com/ada/earthvalley/yomojomo/common/exceptions/GlobalExceptionHandlerTest.java
+++ src/test/java/com/ada/earthvalley/yomojomo/common/exceptions/GlobalExceptionHandlerTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -21,7 +20,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import com.ada.earthvalley.yomojomo.auth.jwt.BearerAuthenticationConverter;
 import com.ada.earthvalley.yomojomo.common.exceptions.handler.GlobalExceptionHandler;
 
-@WebMvcTest
+@WebMvcTest(ExceptionTestController.class)
 class GlobalExceptionHandlerTest {
 	private MockMvc mockMvc;
 	@MockBean
