40aa52c3d469761b81cc53db824ce8e2232c0b3b
LeoBang <bluke8489@gmail.com>
2022-11-17 20:45:12 +0900

    [Refactor] GlobalExceptionHandlerTest 수정 및 테스트 코드 수정 (#59) (As Is) - BearerAuthenticationConverter가 Authorization 헤더가 없을 시 null을 반환함
    (To Be)
    - BearerAuthenticationConverter가 Authorization 헤더가 없을 시 YomojomoAuthException을 던짐

--- src/main/java/com/ada/earthvalley/yomojomo/auth/jwt/BearerAuthenticationConverter.java
+++ src/main/java/com/ada/earthvalley/yomojomo/auth/jwt/BearerAuthenticationConverter.java
@@ -14,24 +14,24 @@ import com.ada.earthvalley.yomojomo.auth.exceptions.YomojomoAuthException;
 // TODO: Exception 수정 (by Leo - 22.10.30)
 @Component
 public class BearerAuthenticationConverter implements AuthenticationConverter {
-    private static final String AUTHENTICATION_SCHEME = "Bearer";
+	private static final String AUTHENTICATION_SCHEME = "Bearer";
 
-    @Override
-    public JwtAuthenticationToken convert(HttpServletRequest request) {
-        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
-        if (header == null) {
-            return null;
-        }
-        header = header.trim();
-        String[] splited = header.split(" ");
-        if (splited.length != 2) {
-            throw new YomojomoAuthException(ILLEGAL_AUTH_HEADER);
-        }
-        final String scheme = splited[0];
-        final String token = splited[1];
-        if (!AUTHENTICATION_SCHEME.equals(scheme)) {
-            throw new YomojomoAuthException(ILLEGAL_AUTH_HEADER);
-        }
-        return JwtAuthenticationToken.unauthenticated(token);
-    }
+	@Override
+	public JwtAuthenticationToken convert(HttpServletRequest request) throws YomojomoAuthException {
+		String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+		if (header == null) {
+			throw new YomojomoAuthException(ILLEGAL_AUTH_HEADER);
+		}
+		header = header.trim();
+		String[] splited = header.split(" ");
+		if (splited.length != 2) {
+			throw new YomojomoAuthException(ILLEGAL_AUTH_HEADER);
+		}
+		final String scheme = splited[0];
+		final String token = splited[1];
+		if (!AUTHENTICATION_SCHEME.equals(scheme)) {
+			throw new YomojomoAuthException(ILLEGAL_AUTH_HEADER);
+		}
+		return JwtAuthenticationToken.unauthenticated(token);
+	}
 }
