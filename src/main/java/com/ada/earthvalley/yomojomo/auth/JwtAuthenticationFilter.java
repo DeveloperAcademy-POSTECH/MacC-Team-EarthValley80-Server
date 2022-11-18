57a84086dfccd30bf3aeecae9cd7fd7f95112fd8
LeoBang <bluke8489@gmail.com>
2022-11-18 23:36:57 +0900

    [Feat] JwtAuthenticationFilter 완성 및 AuthenticationPrincipal 적용 (#59) - JwtAuthenticationFilter 는 DB I/O 없이 토큰 검증만으로 인증을 완료함. - 필요한 인증 과정은 추후에 덧붙이거나 후처리 필터를 이용하겠음. - ErrorResponse의 생성자를 public으로 변경... - AuthenticationPrincipal이 적용된 @YomojomoUser 정의

--- src/main/java/com/ada/earthvalley/yomojomo/auth/JwtAuthenticationFilter.java
+++ src/main/java/com/ada/earthvalley/yomojomo/auth/JwtAuthenticationFilter.java
@@ -7,13 +7,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.stereotype.Component;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.ada.earthvalley.yomojomo.auth.exceptions.YomojomoAuthException;
 import com.ada.earthvalley.yomojomo.auth.jwt.BearerAuthenticationConverter;
 import com.ada.earthvalley.yomojomo.auth.jwt.JwtAuthenticationToken;
 import com.ada.earthvalley.yomojomo.auth.jwt.JwtUtilsService;
 import com.ada.earthvalley.yomojomo.auth.jwt.dtos.YomojomoClaim;
+import com.ada.earthvalley.yomojomo.common.exceptions.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,13 +29,31 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 		FilterChain filterChain) throws ServletException, IOException {
-		JwtAuthenticationToken authentication = converter.convert(request);
-		YomojomoClaim claim = jwtUtilsService.verify(authentication.getCredentials());
-		filterChain.doFilter(request, response);
+		try {
+			JwtAuthenticationToken authentication = converter.convert(request);
+			YomojomoClaim claim = jwtUtilsService.verify(authentication.getCredentials());
+			// TODO: setClaim 시 claim을 등록하면서 authentication = true 로 변경함. 인증 로직 추가 시 확인 요망 (by Leo - 22.11.18)
+			authentication.setClaims(claim);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+			filterChain.doFilter(request, response);
+		} catch (YomojomoAuthException e) {
+			// TODO: AuthenticationEntryPoint 를 이용해 refactoring (by Leo - 22.11.18)
+			sendErrorResponse(response, e);
+		}
 	}
 
 	@Override
 	protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
 		return false;
+	}
+
+	private void sendErrorResponse(HttpServletResponse response, YomojomoAuthException e) throws
+		IOException {
+		response.setStatus(e.getInfo().getStatus().value());
+		response.setContentType("application/json;charset=UTF-8");
+		ErrorResponse errorResponse = new ErrorResponse(e.getInfo());
+		ObjectMapper objectMapper = new ObjectMapper();
+		String serializedString = objectMapper.registerModule(new JavaTimeModule()).writeValueAsString(errorResponse);
+		response.getWriter().print(serializedString);
 	}
 }
