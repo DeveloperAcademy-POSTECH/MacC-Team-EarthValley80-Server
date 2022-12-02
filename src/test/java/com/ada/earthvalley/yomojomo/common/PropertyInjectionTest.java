660ea2abf299435f5ca1b2595eefcccb1da3d7dc
LeoBang <bluke8489@gmail.com>
2022-12-02 17:32:13 +0900

    [Test] 환경변수 테스트 추가 (#80) - kakao 환경변수 테스트

--- src/test/java/com/ada/earthvalley/yomojomo/common/PropertyInjectionTest.java
+++ src/test/java/com/ada/earthvalley/yomojomo/common/PropertyInjectionTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.util.ReflectionTestUtils;
 
@@ -16,6 +17,12 @@ public class PropertyInjectionTest {
 	@Autowired
 	private JwtPropertyService jwtPropertyService;
 
+	@Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+	private String kakaoClientId;
+
+	@Value("${spring.security.oauth2.client.registration.kakao.client-secret}")
+	private String kakaoClientSecret;
+
 	// TODO: @Value 가 main path의 값을 가져오도록 테스트 (by Leo - 22.11.15)
 	@DisplayName("JwtSecretsService 프로퍼티 테스트")
 	@Test
@@ -23,8 +30,25 @@ public class PropertyInjectionTest {
 		String secret = (String)ReflectionTestUtils.getField(jwtPropertyService, "secret");
 		String issuer = jwtPropertyService.getIssuer();
 
+		System.out.println("secret = " + secret);
+		System.out.println("issuer = " + issuer);
+
 		assertThat(jwtPropertyService).isNotNull();
 		assertThat(issuer).isNotNull();
 		assertThat(secret).isNotNull();
+		assertThat(issuer).isNotEqualTo("${JWT_CLAIMS_ISSUER}");
+		assertThat(secret).isNotEqualTo("${JWT_SECRET_KEY}");
+	}
+
+	@DisplayName("OAuth2 ClientRegistration - KAKAO 테스트")
+	@Test
+	void kakaoClients() {
+		System.out.println("kakaoClientId = " + kakaoClientId);
+		System.out.println("kakaoClientSecret = " + kakaoClientSecret);
+
+		assertThat(kakaoClientId).isNotNull();
+		assertThat(kakaoClientSecret).isNotNull();
+		assertThat(kakaoClientId).isNotEqualTo("${SPRING_SECURITY_KAKAO_CLIENT_ID}");
+		assertThat(kakaoClientSecret).isNotEqualTo("${SPRING_SECURITY_KAKAO_CLIENT_SECRET}");
 	}
 }
