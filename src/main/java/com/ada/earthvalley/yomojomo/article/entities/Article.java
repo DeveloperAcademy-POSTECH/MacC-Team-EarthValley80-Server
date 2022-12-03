c01d726655f187da654861f8ab118adf832bca1c
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-12-04 02:43:19 +0900

    [Feat] Article Entity

--- src/main/java/com/ada/earthvalley/yomojomo/article/entities/Article.java
+++ src/main/java/com/ada/earthvalley/yomojomo/article/entities/Article.java
@@ -4,17 +4,35 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import com.ada.earthvalley.yomojomo.common.baseEntities.BaseEntity;
 
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Entity
-public class Article {
+public class Article extends BaseEntity {
 	@Id
 	@GeneratedValue
 	@Column(name = "article_id")
 	private Long id;
+
+	@NotNull
+	private String initialId;
+
+	@NotNull
+	private String title;
+
+	@NotNull
+	private String author;
+
+	@NotNull
+	private String source;
+
+	@NotNull
+	private String majorTopic;
 }
