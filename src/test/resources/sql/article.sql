cf4b32da2f6ba4862e2b0a0a80d79d267bf104b1
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-12-05 01:33:31 +0900

    [Test] article add mock data

---
+++ src/test/resources/sql/article.sql
@@ -0,0 +1,5 @@
+INSERT INTO article (article_id, topic_id, initial_id, image_url, title, author, source, major_topic) VALUES (1, 1, 1234, 'http://kid.chosun.com/site/data/img_dir/2022/12/04/2022120400855_thumb.jpg','제목1', '일일일','어린이 조선', 'CURRENT_AFFAIR');
+INSERT INTO article (article_id, topic_id, initial_id, image_url,title, author, source, major_topic) VALUES (2, 2, 1235, 'http://kid.chosun.com/site/data/img_dir/2022/12/04/2022120400855_thumb.jpg','제목2', '이이이','어린이 조선', 'CURRENT_AFFAIR');
+INSERT INTO article (article_id, topic_id, initial_id, image_url,title, author, source, major_topic) VALUES (3, 3, 1236, 'http://kid.chosun.com/site/data/img_dir/2022/12/04/2022120400855_thumb.jpg','제목3', '삼삼삼','어린이 조선', 'CURRENT_AFFAIR');
+INSERT INTO article (article_id, topic_id, initial_id, image_url,title, author, source, major_topic) VALUES (4, 14, 1237, 'http://kid.chosun.com/site/data/img_dir/2022/12/04/2022120400855_thumb.jpg','제목4', '사사사','어린이 조선', 'CULTURE');
+
