9b1b3a7fba568f76f99fc788932a4a5a02a2eeda
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-12-30 13:28:46 +0900

    [Feat] CreateReadingRequest dto

---
+++ src/main/java/com/ada/earthvalley/yomojomo/activity/dtos/CreateReadingRequest.java
@@ -0,0 +1,10 @@
+package com.ada.earthvalley.yomojomo.activity.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor()
+public class CreateReadingRequest {
+	private String status;
+}
