0826232baa048dccc105eb61306e996cafc7bd2f
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-12-30 14:31:44 +0900

    [Feat] Reaction 남기기 controller

--- src/main/java/com/ada/earthvalley/yomojomo/article/controllers/ArticleController.java
+++ src/main/java/com/ada/earthvalley/yomojomo/article/controllers/ArticleController.java
@@ -1,13 +1,17 @@
 package com.ada.earthvalley.yomojomo.article.controllers;
 
+import java.net.URI;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ada.earthvalley.yomojomo.article.dtos.FetchArticleDetailResponse;
 import com.ada.earthvalley.yomojomo.article.dtos.FetchArticleListResponse;
+import com.ada.earthvalley.yomojomo.article.dtos.SaveReactionRequest;
 import com.ada.earthvalley.yomojomo.article.services.ArticleApiService;
 import com.ada.earthvalley.yomojomo.auth.SecurityUser;
 import com.ada.earthvalley.yomojomo.auth.YomojomoUser;
@@ -34,5 +38,12 @@ public class ArticleController {
 	@GetMapping("/{articleId}")
 	public ResponseEntity<FetchArticleDetailResponse> getArticleDetail(@PathVariable Long articleId) {
 		return ResponseEntity.ok(articleApiService.getArticleDetail(articleId));
+	}
+
+	@PostMapping("/{articleId}/reactions")
+	public ResponseEntity saveUserReactions(@PathVariable Long articleId, @YomojomoUser SecurityUser user,
+		SaveReactionRequest saveReactionRequest) {
+		articleApiService.saveArticleReaction(articleId, user, saveReactionRequest);
+		return ResponseEntity.ok(URI.create("/api/articles/{articleId}/reactions"));
 	}
 }
