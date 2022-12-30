473ace3f6a118507b4624b31574e0c50c85b87a0
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-12-30 13:29:18 +0900

    [Feat] Create reading controller

--- src/main/java/com/ada/earthvalley/yomojomo/activity/controllers/ReadingController.java
+++ src/main/java/com/ada/earthvalley/yomojomo/activity/controllers/ReadingController.java
@@ -1,10 +1,15 @@
 package com.ada.earthvalley.yomojomo.activity.controllers;
 
+import java.net.URI;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ada.earthvalley.yomojomo.activity.dtos.CreateReadingRequest;
 import com.ada.earthvalley.yomojomo.activity.dtos.ReadingListResponse;
 import com.ada.earthvalley.yomojomo.activity.services.ReadingApiService;
 import com.ada.earthvalley.yomojomo.auth.SecurityUser;
@@ -21,5 +26,13 @@ public class ReadingController {
 	@GetMapping("/readings")
 	public ResponseEntity<ReadingListResponse> fetchReadings(@YomojomoUser SecurityUser user) {
 		return ResponseEntity.ok(readingApiService.fetchUsersReading(user));
+	}
+
+	@PostMapping("/articles/{articleId}/reading-contexts")
+	public ResponseEntity<ReadingListResponse> fetchReadings(@YomojomoUser SecurityUser user,
+		CreateReadingRequest createReadingRequest,
+		@PathVariable Long articleId) {
+		Long readingId = readingApiService.createReadingService(user, createReadingRequest, articleId);
+		return ResponseEntity.created(URI.create("/api/articles/" + articleId + "readings/" + readingId)).build();
 	}
 }
